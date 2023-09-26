61651ff3200fefff74cbcd519bf99b8ba185d04b
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-09-27 00:38:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ buttons.forEach(function(button){
           case (e.target.id ==='grey'):
             body.style.backgroundColor = e.target.id;
             break;
-          case (e.target.id ==='blue'):
-            body.style.backgroundColor = e.target.id;
-            break;
+          // case (e.target.id ==='blue'):
+          //   body.style.backgroundColor = e.target.id;
+          //   break;
             default:
               body.style.backgroundColor= e.target.id
               break;
